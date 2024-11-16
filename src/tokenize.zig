d6ac48a7bf132e53ce15fd710d7df2a4ef499976
Jackie Li <jackie.space@gmail.com>
2024-11-16 12:28:36 +0000

    tokenizer

--- src/tokenize.zig
+++ src/tokenize.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
 const print = std.debug.print;
 const eql = std.mem.eql;
-const unicode = @import("std").unicode;
+const unicode = std.unicode;
+const ascii = std.ascii;
+
+const identifier_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+const number_chars = "0123456789";
+const hex_chars = "0123456789abcdefABCDEF";
 
 const modifier_flags_str = [_][]const u8{
     "alt",   "lalt",   "ralt",
@@ -53,7 +58,6 @@ const TokenType = enum {
     Token_EndList,
 
     Token_Unknown,
-    Token_EndOfStream,
 };
 
 const assert = std.debug.assert;
@@ -97,32 +101,33 @@ test "acceptRun" {
 
 test "acceptUntil" {
     const content =
-        \\   hello
+        \\hello \
         \\world
         \\
     ;
     var tokenizer = try Tokenizer.init(content);
-    _ = tokenizer.acceptUntil('\n');
-    try expectEqual(9, tokenizer.cursor);
+    const got = tokenizer.acceptUntil('\n');
+    try expectEqual("hello..|".len, tokenizer.cursor);
     try expectEqual(1, tokenizer.line);
+    try expectEqualStrings("hello \\", got);
 }
 
-// test "tokenize" {
-//     const filename = "/Users/jackieli/.config/skhd/skhdrc";
-//     const allocator = std.testing.allocator;
-//
-//     print("Parsing file: {s}\n", .{filename});
-//     const f = try std.fs.cwd().openFile(filename, .{});
-//     defer f.close();
-//
-//     const content = try f.readToEndAlloc(allocator, 1 << 24); // max size 16MB
-//     defer allocator.free(content);
-//
-//     var tokenizer = try Tokenizer.init(content);
-//     while (tokenizer.nextRune()) |rune| {
-//         print("{s}", .{rune});
-//     }
-// }
+test "tokenize" {
+    const filename = "/Users/jackieli/.config/skhd/skhdrc";
+    const allocator = std.testing.allocator;
+
+    print("Parsing file: {s}\n", .{filename});
+    const f = try std.fs.cwd().openFile(filename, .{});
+    defer f.close();
+
+    const content = try f.readToEndAlloc(allocator, 1 << 24); // max size 16MB
+    defer allocator.free(content);
+
+    var tokenizer = try Tokenizer.init(content);
+    while (tokenizer.get_token()) |token| {
+        print("line: {d}, cursor: {d}, type: {any}, text: {s}\n", .{ token.line, token.cursor, token.type, token.text });
+    }
+}
 
 const Token = struct {
     type: TokenType,
@@ -152,7 +157,96 @@ const Tokenizer = struct {
         };
     }
 
-    // pub fn get_token(self: *Self) Token {}
+    pub fn get_token(self: *Self) ?Token {
+        self.skipWhitespace();
+
+        var token = Token{
+            .line = self.line,
+            .cursor = self.cursor,
+            .type = .Token_Unknown,
+            .text = undefined,
+        };
+
+        const r = self.peekRune() orelse return null;
+        self.moveOver(r);
+        token.text = r;
+        switch (r[0]) {
+            '+' => token.type = .Token_Plus,
+            ',' => token.type = .Token_Comma,
+            '<' => token.type = .Token_Insert,
+            '@' => token.type = .Token_Capture,
+            '~' => token.type = .Token_Unbound,
+            '*' => token.type = .Token_Wildcard,
+            '[' => token.type = .Token_BeginList,
+            ']' => token.type = .Token_EndList,
+            '.' => {
+                token.type = .Token_Option;
+                token.text = self.acceptUntil(' ');
+            },
+            '"' => {
+                token.type = .Token_String;
+                // TODO: handle escape characters
+                token.text = self.acceptUntil('"');
+                self.moveOver("\"");
+            },
+            '#' => {
+                _ = self.acceptUntil('\n');
+                token = self.get_token() orelse return null;
+            },
+            '-' => {
+                if (self.accept(">") != null) {
+                    token.type = .Token_Arrow;
+                    token.text = "->";
+                } else {
+                    token.type = .Token_Dash;
+                }
+            },
+            ';' => {
+                self.skipWhitespace();
+                token.type = .Token_Activate;
+                token.line = self.line;
+                token.cursor = self.cursor;
+                token.text = self.acceptIdentifier();
+            },
+            ':' => {
+                if (self.accept(":") != null) {
+                    token.type = .Token_Decl;
+                    token.text = "::";
+                } else {
+                    self.skipWhitespace();
+                    token.line = self.line;
+                    token.cursor = self.cursor;
+                    token.type = .Token_Command;
+                    token.text = self.acceptCommand();
+                }
+            },
+            '|' => {
+                self.skipWhitespace();
+                token.line = self.line;
+                token.cursor = self.cursor;
+                token.type = .Token_Forward;
+            },
+            else => {
+                const next = self.peekRune() orelse return null;
+                if (r[0] == '0' and next[0] == 'x') {
+                    self.moveOver(next);
+                    token.text = self.acceptRun(hex_chars);
+                    token.type = .Token_Key_Hex;
+                } else if (ascii.isDigit(r[0])) {
+                    token.type = .Token_Key;
+                } else if (ascii.isAlphanumeric(r[0])) {
+                    const start = self.pos - 1; // rewind
+                    _ = self.acceptIdentifier();
+                    const end = self.pos;
+                    token.text = self.buffer[start..end];
+                    token.type = resolveIdentifierType(token);
+                } else {
+                    token.type = .Token_Unknown;
+                }
+            },
+        }
+        return token;
+    }
     //
     // pub fn peek_token(self: *Self) Token {}
 
@@ -175,38 +269,81 @@ const Tokenizer = struct {
 
     /// Accept consumes the next rune if it's in the valid set.
     /// valid only accepts ASCII characters.
-    fn accept(self: *Self, cs: []const u8) bool {
-        const r = self.peekRune() orelse return false;
-        for (cs) |accepted| {
-            if (accepted == r[0]) {
+    fn accept(self: *Self, validSet: []const u8) ?[]const u8 {
+        const r = self.peekRune() orelse return null;
+        for (validSet) |valid| {
+            if (valid == r[0]) {
                 self.moveOver(r);
-                return true;
+                return r;
             }
         }
-        return false;
+        return null;
     }
 
-    fn acceptRun(self: *Self, cs: []const u8) ?[]const u8 {
+    fn acceptRun(self: *Self, cs: []const u8) []const u8 {
         const start = self.pos;
-        while (self.accept(cs)) {}
+        while (self.accept(cs)) |_| {}
         const end = self.pos;
         return self.buffer[start..end];
     }
 
-    fn acceptUntil(self: *Self, cs: u8) bool {
-        while (true) {
-            const r = self.peekRune() orelse return false;
+    fn acceptUntil(self: *Self, cs: u8) []const u8 {
+        const start = self.pos;
+        while (self.peekRune()) |r| {
             if (r[0] == cs) {
-                return true;
+                break;
             }
             self.moveOver(r);
         }
+        return self.buffer[start..self.pos];
+    }
+
+    fn acceptIdentifier(self: *Self) []const u8 {
+        const start = self.pos;
+        _ = self.acceptRun(identifier_chars);
+        _ = self.acceptRun(identifier_chars ++ number_chars);
+        const end = self.pos;
+        return self.buffer[start..end];
+    }
+
+    fn acceptCommand(self: *Self) []const u8 {
+        const start = self.pos;
+        while (self.peekRune()) |r| {
+            self.moveOver(r);
+            if (r[0] == '\\') {
+                _ = self.accept("\n");
+            }
+            if (self.accept("\n") != null) {
+                break;
+            }
+        }
+        return self.buffer[start..self.pos];
     }
 
     fn skipWhitespace(self: *Self) void {
-        self.acceptRun(" \t\n");
+        _ = self.acceptRun(" \t\n");
     }
 };
+
+fn resolveIdentifierType(token: Token) TokenType {
+    if (token.text.len == 1) {
+        return .Token_Key;
+    }
+
+    for (modifier_flags_str) |modifier| {
+        if (eql(u8, modifier, token.text)) {
+            return .Token_Modifier;
+        }
+    }
+
+    for (literal_keycode_str) |keycode| {
+        if (eql(u8, keycode, token.text)) {
+            return .Token_Literal;
+        }
+    }
+
+    return .Token_Identifier;
+}
 
 // struct token
 // {
